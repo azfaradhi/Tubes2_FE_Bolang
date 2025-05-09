c75086530e9a63c8f92cb650cbd1a8f3d252bb3c
azfaradhi <aradhihakim@gmail.com>
2025-05-09 20:59:36 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['static.wikia.nocookie.net'],
+  },
 };
 
 export default nextConfig;
